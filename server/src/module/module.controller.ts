11ca95f7b6e08829151ee0730e6f7814adf41977
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2022-07-17 11:45:55 +0700

    create path url query all submodule

--- server/src/module/module.controller.ts
+++ server/src/module/module.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { ModuleService } from './module.service';
+import { SubmoduleService } from './submodule.service';
 
 @Controller('module')
 export class ModuleController {
-    constructor(private readonly sectionService: ModuleService) { }
+    constructor(
+        private readonly sectionService: ModuleService,
+        private readonly subModuleService: SubmoduleService
+    ) { }
 
     @Get()
-    findAll() {
+    findAllModule() {
         return this.sectionService.findAll();
+    }
+
+    @Get('/submodule')
+    findAllSubModule() {
+        return this.subModuleService.findAll();
     }
 }
